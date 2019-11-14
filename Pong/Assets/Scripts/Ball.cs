ac5d15fec519d46ed199d38306cbea512b646bc6
Toby Klauder <toby.klauder@digipen.edu>
2019-11-14 08:59:52 -0800

    sound omg it works

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events; 
 
 
 public class Ball : MonoBehaviour
 {
+    public UnityEvent onhit;
+    public UnityEvent ongoal; 
     public Shakebehavior sb;
     public GameObject trigger;
-    public GameObject ball; 
-    public AudioClip hitplayer; // start dev on sound triggers 
+    public GameObject ball;  // start dev on sound triggers 
     public float speed = 5; 
     public int ballactivate = 0; 
     public int playerone, playertwo = 0; 
@@ -23,7 +24,7 @@ public class Ball : MonoBehaviour
         colorlist[1] = Color.green;
         colorlist[2] = Color.magenta;
         colorlist[3] = Color.yellow;
-        colorlist[4] = Color.grey; 
+        colorlist[4] = Color.grey;
         rb = GetComponent<Rigidbody2D>();
         ballactivate = 0;
         Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10f));
@@ -60,11 +61,13 @@ public class Ball : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "PlayerOneS" || collision.gameObject.name == "PlayerTwoS" || collision.gameObject.name == "PlayerThreeS" || collision.gameObject.name == "PlayerFourS") {
-            Start(); 
+            Start();
+            ongoal.Invoke(); 
         }
         if (collision.gameObject.name == "PlayerOne" || collision.gameObject.name == "PlayerTwo") {
             speed += 1;
             sb.shakeDuration = 0.5f;
+            onhit.Invoke(); 
             var ballrenderer = ball.GetComponent<Renderer>();
             if (colorcount == 5) {
                 colorcount = 0; 
